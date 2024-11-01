ded984770e6901aea3bb0e177e96a88eaf95076b
Anna-1987 <mag260910@gmail.com>
2024-11-01 15:14:20 +0200

    ctepen

--- 09-input.js
+++ 09-input.js
@@ -2,7 +2,17 @@
 // console.log(shouldRenew);
 
 
-let quantity = prompt('введите количество товаров');
-quantity = Number(quantity); //преобразовывает строку в число, если это возможно 
-console.log(quantity);
-console.log(typeof quantity);//typeof возвращает тип переменной
+// let quantity = prompt('введите количество товаров');
+// quantity = Number(quantity); //преобразовывает строку в число, если это возможно 
+// console.log(quantity);
+// console.log(typeof quantity);//typeof возвращает тип переменной
+
+//------------Задача возведения в степень
+let number = prompt('введите число');
+
+let degree = prompt('введите степень');
+
+const result = number ** degree;
+
+const message = confirm(result);
+console.log(result);
