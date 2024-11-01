94d2180c97e59bebc90391eb4a6bc7d28fb33b97
Anna-1987 <mag260910@gmail.com>
2024-11-01 14:42:01 +0200

    input

---
+++ 09-input.js
@@ -0,0 +1,8 @@
+// const shouldRenew = confirm('Хотите ли вы продлить подписку');
+// console.log(shouldRenew);
+
+
+let quantity = prompt('введите количество товаров');
+quantity = Number(quantity); //преобразовывает строку в число, если это возможно 
+console.log(quantity);
+console.log(typeof quantity);//typeof возвращает тип переменной
